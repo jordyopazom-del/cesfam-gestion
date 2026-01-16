6f7ef4809ddef03bc68b4780ed1c3b5b9e92feba
kkoandres-byte <kkoandres@gmail.com>
2026-01-16 08:25:07 -0300

    Add Licencia Medica to request types

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -13,6 +13,7 @@ export const COORDINATORS = [
 export const BLOCK_TYPES = [
     "Otros",
     "Feriado Legal",
+    "Licencia Medica",
     "Permiso Administrativo",
     "Permiso Compensatorio",
     "Permiso sin goce",
